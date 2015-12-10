c6635becaea86ed3beea6916f8bbe83280177d52
Dittaphong Nilnama <aostudio.nilnama@gmail.com>
2015-12-11 00:16:34 +0700

    MASTER - Employee

--- application/views/employee/D001.php
+++ application/views/employee/D001.php
@@ -2,35 +2,156 @@
 foreach ($listemployee as $empHdr)
 {
 ?>
-<div class="form_input">
+
+<div class="row form_input" style="text-align:left; margin-bottom:20px">
+	<div class="col-md-3" >
 		<p>รหัสพนักงาน</p>
-		<input type="text" class="input" name="memp_code" value="<?php echo $empHdr->memp_code;   ?>" readonly="true">
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="memp_code" required > 
+	</div>
+	<div class="col-md-3" >
+		<p>ตำแหน่ง</p>
+		<p class="required">*</p>
+		<select name="id_mpst" class ="form-control" required>
+			<option value="">--เลือก--</option> 
+			<?php 
+			foreach ($listMpst as $Mpst)
+			{ 
+				echo "<option value='".$Mpst->id_mpst."'>".$Mpst->name_th."</option>";
+			}
+			?>
+		</select>
+	</div>
+	<div class="col-md-3" >
+		<p>สำนักงาน/สาขา</p>
+		<p class="required">*</p>
+		<select name="id_mdept" class ="form-control" required>
+			<option value="">--เลือก--</option> 
+			<?php 
+			foreach ($listMdept as $Mdept)
+			{ 
+				echo "<option value='".$Mdept->id_mdept."'>".$Mdept->name_th."</option>";
+			}
+			?>
+		</select>
+	</div>
+	<div class="col-md-3" >
+		<p>เลขประจำตัวประชาชน</p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="idcard_num"  required>
+	</div>
+	<div class="col-md-3" >
+		<p>คำนำหน้าชื่อ</p>
+		<select name="id_memp_tit" class ="form-control" required>
+			<option value="">--เลือก--</option> 
+			<option value="1"> นาย </option>
+			<option value="2"> นาง </option>
+			<option value="3"> นางสาว </option>
+		</select> 
+	</div>
+	<div class="col-md-3" >
+		<p>ชื่อ </p>
+		<p class="required">*</p>
+		<input type="text" class="form-control"  name="firstname_th" placeholder="ชื่อ" required> 
+	</div>
+	<div class="col-md-3" >
+		<p>นามสกุล </p>
+		<p class="required">*</p> 
+		<input type="text" class="form-control"  name="lastname_th" placeholder="สกุล" required>
+	</div>
+	<div class="col-md-3" >
+		<p>วันเกิด</p>
+		<p class="required"></p>
+		<input type="text" class="form-control" name="birthdate" id="birthdate"  >
+	</div> 
+	<div class="col-md-3" >
+		<p>เลขใบอนุญาตขับขี่</p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="drv_lcn_num" >
+	</div>
+	<div class="col-md-3" >
+		<p>อีเมลล์ <b ID="valid_email"></b></p>
+		<p class="required">*</p>
+		<input type="email" class="form-control" name="email" ID="email" >
+	</div>
+	<div class="col-md-3" >
+		<p>โทรศัพท์</p>
+		<input type="text" class="form-control" name="telephone"  >
+	</div>
+	<div class="col-md-3" >
+		<p>มือถือ <b ID="valid_mobile"></b></p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" ID="mobile" name="mobile" >
+	</div>
+	<div class="col-md-3" >
+		<p>แฟกซ์</p>
+		<input type="text" class="form-control" name="fax" >
+	</div>
+	<div class="col-md-3" >
+		<p>ชื่อเข้าใช้ระบบ <b ID="valid"></b></p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="username" ID="user" required>
+	</div>
+	<div class="col-md-3" >
+		<p>รหัสผ่าน</p>
+		<p class="required">*</p>
+		<input type="password" class="form-control" name="userpassword" id="userpassword" required>
+	</div>
+	<div class="col-md-3" >
+		<p>ยืนยันรหัสผ่าน</p>
+		<p class="required">*</p>
+		<input type="password" class="form-control" name="con_pass" id="confirmpw" required>
+	</div>
+	<div class="col-md-6" >
+		<p>ที่อยู่ (ตามสำเนาทะเบียนบ้าน)</p>
+		<p class="required">*</p>
+		<textarea  class="form-control" rows='3' name="adr_line1" required></textarea>
+	</div>
+	<div class="col-md-6" >
+		<p>ที่อยู่ (ปัจจุบัน)</p>
+		<p class="required">*</p>
+		<textarea  class="form-control" rows='3' name="adr_line2" ></textarea> 
+	</div>
+	<div class="col-md-12" >
+		<p>หมายเหตุ</p>
+		<textarea  class="form-control" rows='3' name="comment"></textarea>
+	</div>
+</div>
+
+
+<!-- 
+<div class="row form_input" style="text-align:left; margin-bottom:20px">
+	<div class="col-md-3" >
+		<p>รหัสพนักงาน</p>
+		<input type="text" class="input" name="mmember_code" value="<?php echo $empHdr->mmember_code;   ?>" readonly="true">
+	</div>
+	<div class="col-md-3" >
 		<p>ตำแหน่ง</p>
 		<select name="id_mpst" class ="select" disabled>
 			<option value="">--เลือก--</option> 
 			<?php 
-			foreach ($listMpst as $Mpst)
+			foreach ($listMposition as $Mpst)
 			{
-				$c= "<option value='".$Mpst->id_mpst."' ";
-				if($empHdr->id_mpst==$Mpst->id_mpst){
+				$c= "<option value='".$Mpst->id_mposition."' ";
+				if($empHdr->id_mposition==$Mpst->id_mposition){
 					$c .= "selected";
 				}
-				$c .=">".$Mpst->name_th."</option>";
+				$c .=">".$Mpst->mposition_name."</option>";
 				echo $c;
 			}
 			?>
 		</select>
 		<p>แผนก</p>
-		<select name="id_mdept" class ="select" disabled>
+		<select name="id_mbranch" class ="select" disabled>
 			<option value="">--เลือก--</option> 
 			<?php 
-			foreach ($listMdept as $Mdept)
+			foreach ($listMbranch as $Mbranch)
 			{
-				$c= "<option value='".$Mdept->id_mdept."' ";
-				if($empHdr->id_mdept==$Mdept->id_mdept){
+				$c= "<option value='".$Mbranch->id_mbranch."' ";
+				if($empHdr->id_mbranch==$Mbranch->id_mbranch){
 					$c .= "selected";
 				}
-				$c .=">".$Mdept->name_th."</option>";
+				$c .=">".$Mbranch->mbranch_name."</option>";
 				echo $c;
 			}
 			?>
@@ -42,10 +163,10 @@ foreach ($listemployee as $empHdr)
 			<option value="2" <?php if($empHdr->sex=='2'){ echo "selected"; } ?>> หญิง </option> 
 		</select> 
 		<p>คำนำหน้าชื่อ</p>
-		<select name="id_memp_tit" class ="select" disabled>
-			<option value="1" <?php if($empHdr->id_memp_tit=='1'){ echo "selected"; } ?>> นาย </option>
-			<option value="2" <?php if($empHdr->id_memp_tit=='2'){ echo "selected"; } ?>> นาง </option>
-			<option value="3" <?php if($empHdr->id_memp_tit=='3'){ echo "selected"; } ?>> นางสาว </option>
+		<select name="id_mmember_tit" class ="select" disabled>
+			<option value="1" <?php if($empHdr->id_mmember_tit=='1'){ echo "selected"; } ?>> นาย </option>
+			<option value="2" <?php if($empHdr->id_mmember_tit=='2'){ echo "selected"; } ?>> นาง </option>
+			<option value="3" <?php if($empHdr->id_mmember_tit=='3'){ echo "selected"; } ?>> นางสาว </option>
 		</select> 
  		<p>ชื่อพนักงาน(TH)</p>
 		<input type="text" class="inputname" name="firstname_th" value="<?php echo $empHdr->firstname_th;   ?>" placeholder="ชื่อ" readonly required>
@@ -99,7 +220,7 @@ foreach ($listemployee as $empHdr)
 		<input type="text" class="input" name="name_update" value="<?php echo $empHdr->name_update; ?>" disabled>
 		<p>วันที่แก้ไข</p>
 		<input type="text" class="input" name="dt_update" value="<?php echo $empHdr->dt_update; ?>" disabled>
- </div>
+ </div> -->
  <?php 
 }
 ?>
--- application/views/employee/E001.php
+++ application/views/employee/E001.php
@@ -93,7 +93,124 @@ function updateMemp()
     });
 }
 </script>
-<?php  foreach ($listMemp as $detail) { ?>
+<?php  foreach ($listemployee as $detail) { ?>
+
+<div class="row form_input" style="text-align:left; margin-bottom:20px">
+	<div class="col-md-3" >
+		<p>รหัสพนักงาน</p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="memp_code" required > 
+	</div>
+	<div class="col-md-3" >
+		<p>ตำแหน่ง</p>
+		<p class="required">*</p>
+		<select name="id_mpst" class ="form-control" required>
+			<option value="">--เลือก--</option> 
+			<?php 
+			foreach ($listMpst as $Mpst)
+			{ 
+				echo "<option value='".$Mpst->id_mpst."'>".$Mpst->name_th."</option>";
+			}
+			?>
+		</select>
+	</div>
+	<div class="col-md-3" >
+		<p>สำนักงาน/สาขา</p>
+		<p class="required">*</p>
+		<select name="id_mdept" class ="form-control" required>
+			<option value="">--เลือก--</option> 
+			<?php 
+			foreach ($listMdept as $Mdept)
+			{ 
+				echo "<option value='".$Mdept->id_mdept."'>".$Mdept->name_th."</option>";
+			}
+			?>
+		</select>
+	</div>
+	<div class="col-md-3" >
+		<p>เลขประจำตัวประชาชน</p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="idcard_num"  required>
+	</div>
+	<div class="col-md-3" >
+		<p>คำนำหน้าชื่อ</p>
+		<select name="id_memp_tit" class ="form-control" required>
+			<option value="">--เลือก--</option> 
+			<option value="1"> นาย </option>
+			<option value="2"> นาง </option>
+			<option value="3"> นางสาว </option>
+		</select> 
+	</div>
+	<div class="col-md-3" >
+		<p>ชื่อ </p>
+		<p class="required">*</p>
+		<input type="text" class="form-control"  name="firstname_th" placeholder="ชื่อ" required> 
+	</div>
+	<div class="col-md-3" >
+		<p>นามสกุล </p>
+		<p class="required">*</p> 
+		<input type="text" class="form-control"  name="lastname_th" placeholder="สกุล" required>
+	</div>
+	<div class="col-md-3" >
+		<p>วันเกิด</p>
+		<p class="required"></p>
+		<input type="text" class="form-control" name="birthdate" id="birthdate"  >
+	</div> 
+	<div class="col-md-3" >
+		<p>เลขใบอนุญาตขับขี่</p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="drv_lcn_num" >
+	</div>
+	<div class="col-md-3" >
+		<p>อีเมลล์ <b ID="valid_email"></b></p>
+		<p class="required">*</p>
+		<input type="email" class="form-control" name="email" ID="email" >
+	</div>
+	<div class="col-md-3" >
+		<p>โทรศัพท์</p>
+		<input type="text" class="form-control" name="telephone"  >
+	</div>
+	<div class="col-md-3" >
+		<p>มือถือ <b ID="valid_mobile"></b></p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" ID="mobile" name="mobile" >
+	</div>
+	<div class="col-md-3" >
+		<p>แฟกซ์</p>
+		<input type="text" class="form-control" name="fax" >
+	</div>
+	<div class="col-md-3" >
+		<p>ชื่อเข้าใช้ระบบ <b ID="valid"></b></p>
+		<p class="required">*</p>
+		<input type="text" class="form-control" name="username" ID="user" required>
+	</div>
+	<div class="col-md-3" >
+		<p>รหัสผ่าน</p>
+		<p class="required">*</p>
+		<input type="password" class="form-control" name="userpassword" id="userpassword" required>
+	</div>
+	<div class="col-md-3" >
+		<p>ยืนยันรหัสผ่าน</p>
+		<p class="required">*</p>
+		<input type="password" class="form-control" name="con_pass" id="confirmpw" required>
+	</div>
+	<div class="col-md-6" >
+		<p>ที่อยู่ (ตามสำเนาทะเบียนบ้าน)</p>
+		<p class="required">*</p>
+		<textarea  class="form-control" rows='3' name="adr_line1" required></textarea>
+	</div>
+	<div class="col-md-6" >
+		<p>ที่อยู่ (ปัจจุบัน)</p>
+		<p class="required">*</p>
+		<textarea  class="form-control" rows='3' name="adr_line2" ></textarea> 
+	</div>
+	<div class="col-md-12" >
+		<p>หมายเหตุ</p>
+		<textarea  class="form-control" rows='3' name="comment"></textarea>
+	</div>
+</div>
+
+<!-- 
 <input type="hidden"  name="base_url" ID="base_url" value="<?php echo base_url(); ?>">
 <input type="hidden"  ID="idx" value="<?php echo $idx; ?>" >
 <div class="form_input">
@@ -203,5 +320,5 @@ function updateMemp()
 	<input type="text" class="input" name="name_update" value="<?php echo $detail->name_update; ?>" readonly>
 	<p>วันที่แก้ไข</p>
 	<input type="text" class="input" name="dt_update" value="<?php echo $detail->dt_update; ?>" readonly>
-</div>
+</div> -->
 <?php } ?> 
